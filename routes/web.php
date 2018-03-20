ce888562d90d0fb13657132b814872a29d9cdfeb
AesopL <wenduan@outlook.com>
2018-03-20 03:17:23 +0000

    Finish user signup

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,6 @@ Route::get('/', 'StaticPagesController@home')->name('home');
 Route::get('/help', 'StaticPagesController@help')->name('help');
 Route::get('/about', 'StaticPagesController@about')->name('about');
 Route::get('signup', 'UsersController@create')->name('signup');
+Route::get('signin', 'UsersController@create')->name('signin');
+// Route::resource('users', 'UsersController');
+Route::resource('users', 'UsersController');

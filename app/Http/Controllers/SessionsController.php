c1caee7884f074864e78419ba8215cbde327473a
AesopL <wenduan@outlook.com>
2018-03-22 05:50:44 +0000

    delete some code

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -27,7 +27,6 @@ class SessionsController extends Controller
             session()->flash('danger', '您的邮箱和密码不匹配');
             return redirect()->back();
         }
-        return;
     }
 
     //退出

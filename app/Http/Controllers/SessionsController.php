1c5a86993355bbedbcd3b702b2e18040a749caca
AesopL <wenduan@outlook.com>
2018-03-22 05:29:33 +0000

    Finish login and logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Auth;
+use Illuminate\Http\Request;
+
+class SessionsController extends Controller
+{
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    public function store(Request $request)
+    {
+        $credentials = $this->validate($request, [
+            'email' => 'required|email|max:255',
+            'password' => 'required',
+        ]);
+        //attempt()方法判断登陆
+        if (Auth::attempt($credentials, $request->has('remember'))) {
+            //登陆成功后
+            session()->flash('success', '欢迎回来');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            session()->flash('danger', '您的邮箱和密码不匹配');
+            return redirect()->back();
+        }
+        return;
+    }
+
+    //退出
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出');
+        return redirect('login');
+    }
+}

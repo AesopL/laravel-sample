7574a18fcf61ec0922513d65b0835f0dd0de1928
AesopL <wenduan@outlook.com>
2018-04-04 01:59:51 +0000

    activation email ,some css edit

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -1,33 +1,34 @@
 @extends('layouts.default')
-
 @section('title','登陆')
 @section('content')
-	<div class="col-md-offset-2 col-md-8">
-		<div class="panel panel-default">
-			<div class="panel-heading">
-				<h5>登陆</h5>
-			</div>
-			<div class="panel-body">
-				@include('shared._errors')
-				<form action="{{ route('login') }}" method="post">
-					{{ csrf_field() }}
-					<div class="from-group">
-						<label for="email">邮箱</label>
-						<input type="text" name="email" class="form-control" value="{{ old('email')}}">
-					</div>
-					<div class="form-group">
-						<label for="password">密码</label>
-						<input type="password" name="password" class="form-control" value="{{ old('password') }}">
-					</div>
-
-					<div class="checkbox">
-						<label><input type="checkbox" name="remember">记住我</label>
-					</div>
-					<button type="submit" class="btn btn-primary">登陆</button>
-					<hr>
-					<p>还没有账号？<a href="{{ route('signup') }}">注册一个吧</a></p>
-				</form>
-			</div>
-		</div>
-	</div>
+<div class="col-md-offset-2 col-md-8">
+    <div class="card">
+        <div class="card-header text-center">
+            <h3>登陆</h3>
+        </div>
+        <div class="card-body">
+            <div class="m-3">
+                @include('shared._errors')
+                <form action="{{ route('login') }}" method="post">
+                    {{ csrf_field() }}
+                    <div class="md-form">
+                        <label for="email" class="ml-2">邮箱</label>
+                        <input type="text" name="email" class="form-control" value="{{ old('email')}}">
+                    </div>
+                    <div class="md-form">
+                        <label for="password">密码</label>
+                        <input type="password" name="password" class="form-control" value="{{ old('password') }}">
+                    </div>
+                    <div class="form-check">
+                    	<input type="checkbox" id="inlineFormCheckMD" class="form-check-input" name="remember">
+                        <label class="form-check-label" for="inlineFormCheckMD">记住我</label>
+                    </div>
+                    <button type="submit" class="btn btn-primary">登陆</button>
+                    <hr>
+                    <p>还没有账号？<a href="{{ route('signup') }}">注册一个吧</a></p>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
 @stop
